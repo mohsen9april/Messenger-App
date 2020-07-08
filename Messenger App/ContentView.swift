280162ddb3f2279e1830f2764d86e78f1c39a5ca
Mohsen Abdollahi <mohsen9april@gmail.com>
2020-07-08 23:06:35 +0430

    Initial Commit

---
+++ Messenger App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Messenger App
+//
+//  Created by Mohsen Abdollahi on 7/8/20.
+//  Copyright © 2020 Mohsen Abdollahi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
